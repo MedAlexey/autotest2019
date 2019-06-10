e13c7f8edb2826366bf923c2497839a7fb456c5a
Alexey <medvedev.lasta@yandex.ru>
2019-06-11 02:15:48 +0700

    add asserts to check() method add comments to methods

--- src/test/java/pages/ShareWithMessageFrame.java
+++ src/test/java/pages/ShareWithMessageFrame.java
@@ -1,11 +1,15 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ShareWithMessageFrame extends BasePage implements IShareFrame {
 
-    private static final By PORTLET_NAME = By.xpath(".//*[@class='portlet_h_name_t']");
+    private static final By PORTLET_NAME =
+            By.xpath(".//*[@class='portlet_h_name_t' and contains(text(), 'Отправить сообщением')]");
     private static final By CLOSE_ICON = By.xpath(".//*[@class='ic modal-new_close_ico']");
     private static final By POSTING_SUBMIT_BUTTON = By.xpath(".//*[@class='posting_submit button-pro']");
     private static final By SEARCH_INPUT = By.xpath(".//*[@class='it_w search-input']");
@@ -16,22 +20,26 @@ public class ShareWithMessageFrame extends BasePage implements IShareFrame {
         super(driver);
     }
 
+    // выбираем друга из списка
     public void choosePerson(String name) {
-        driver.findElement(SEARCH_INPUT).click();
+        click(SEARCH_INPUT);
         driver.findElement(
                 By.xpath(".//*[@class='ucard-mini_cnt_i ellip' and contains(text(), '" + name + "')]")).click();
     }
 
+    // закрыть данный фрейм
     @Override
     public void closeFrame() {
         click(CLOSE_ICON);
     }
 
+    // нажать кнопку "поделиться"
     @Override
     public void share() {
         click(POSTING_SUBMIT_BUTTON);
     }
 
+    // написать текст в поле ввода комментария
     @Override
     public void writeText(String text) {
         sendKeys(TEXT_HOLDER, text);
@@ -39,9 +47,20 @@ public class ShareWithMessageFrame extends BasePage implements IShareFrame {
 
     @Override
     void check(WebDriver driver) {
-        driver.findElement(PORTLET_NAME);
-        driver.findElement(CLOSE_ICON);
-        driver.findElement(POSTING_SUBMIT_BUTTON);
-        driver.findElement(TEXT_HOLDER);
+        Assert.assertTrue( "Не дождались названия фрейма",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(PORTLET_NAME)));
+
+        Assert.assertTrue( "Не дождались кнопки сзакрытия фрейма",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(CLOSE_ICON)));
+
+        Assert.assertTrue( "Не дождались кнопки \"поделиться\"",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(POSTING_SUBMIT_BUTTON)));
+
+        Assert.assertTrue( "Не дождались поля ввода комментария",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(TEXT_HOLDER)));
     }
 }
