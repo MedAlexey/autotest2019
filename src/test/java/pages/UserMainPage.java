18a1e1b19908f7433416984f1ae50b414c43dca2
nik27090 <nik27090@mail.ru>
2019-06-11 23:10:07 +0300

    add commets, changed xpath and openShareFromUserMainPage()

--- src/test/java/pages/UserMainPage.java
+++ src/test/java/pages/UserMainPage.java
@@ -7,33 +7,30 @@ import org.openqa.selenium.WebDriver;
 
 public class UserMainPage extends BasePage {
 
-    private static final By GOODS_BUTTON = By.xpath(".//*[contains(text(),'Товары') and contains(@class,'tico null')]");
+    private static final By GOODS_BUTTON = By.xpath(".//*[@class = 'tico null' and contains(text(),'Товары')]");
     private static final By MESSAGES_BUTTON = By.xpath(".//* [@id='msg_toolbar_button']");
     private static final By CHECK_PRODUCT = By.xpath(".//* [@class='mall-media-link_a']");
-    private static final By REALLY_PRODUCT_NAME = By.xpath(".//* [@class='mall-card_t mb-4x']");
-    private static final By CLOSE_FRAME_BUTTON = By.xpath(".//* [@class='ic modal-new_close_ico']");
 
     UserMainPage(WebDriver driver) {
         super(driver);
     }
 
+    //Открываем страницу с товароми
     public GoodsPage openGoodsPage(){
         click(GOODS_BUTTON);
         return new GoodsPage(driver);
     }
 
+    //открываем страницу с сообщениями
     public MessagesPage openMessagesPage(){
         click(MESSAGES_BUTTON);
         return new MessagesPage(driver);
     }
 
-    public boolean checkProductOnUserMainPage(String expectedProduct){
+    //открываем репост товара со стены юзера
+    public ProductPageFrame openShareFromUserMainPage(){
         click(CHECK_PRODUCT);
-        if (driver.findElement(REALLY_PRODUCT_NAME).getText().equals(expectedProduct)){
-            click(CLOSE_FRAME_BUTTON);
-            return true;}
-        click(CLOSE_FRAME_BUTTON);
-        return false;
+        return new ProductPageFrame(driver);
     }
 
     @Override
