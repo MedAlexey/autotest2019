463cf7190c14073f4e253eab2eb2f449c7e2a350
Lizka <kuzo-liza@yandex.ru>
2019-06-12 13:26:26 +0400

    #57 #5

--- src/test/java/pages/GroupsPage.java
+++ src/test/java/pages/GroupsPage.java
@@ -1,9 +1,12 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class GroupsPage extends BasePage{
+public class GroupsPage extends BasePage {
 
     private static final By GROUPS_MODERATION = By.xpath(".//*[@class='hookBlock' and contains(text(), 'Модерирую')]");
     private static final By GROUP = By.xpath(".//*[@class='stub-img stub-group-interest-128 stub-img__128']");
@@ -20,9 +23,16 @@ public class GroupsPage extends BasePage{
         return new GroupPage(driver);
     }
 
+    // проверка наличия и видимости кнопок
     @Override
-    void check(WebDriver driver) {
-        isElementPresent(GROUPS_MODERATION);
-        isElementPresent(GROUP);
+    protected void check(WebDriver driver) {
+        Assert.assertTrue("Не дождались кнопки 'Модерирую'",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(GROUPS_MODERATION)));
+
+        Assert.assertTrue("Не дождались кнопки перехода в свою группу",
+                new WebDriverWait(driver, 10).
+                        until((ExpectedCondition<Boolean>) d -> isElementPresent(GROUP)));
+
     }
 }
